b720f226907b9599d99be493d0fad7445ff376a8
hr-meheraj <hrmeheraj2021@gmail.com>
2022-08-19 18:14:51 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Navbar from './shared/Navbar';
 function App() {
   return (
-    <div className="App">
-    
+    <div>
+      <header class="navbar-light navbar-sticky header-static">
+        <Navbar />
+      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../shared/Footer'
+import Main from './Main'
+
+function Home() {
+  return (
+    <div>
+        <Main/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
